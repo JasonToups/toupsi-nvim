f35abe348667566d8aea78899f2ed7e2a4f9b4f1
jasontoups <me@jasontoups.com>
2025-08-26 10:28:23 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Toupsi's NeoVim Config
+
+This is where I'm tracking my Nvim config, so I don't bork it too much.
---
+++ lua/theprimeagen/plugins.lua
@@ -0,0 +1,77 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 35,            -- set tree width
+          side = "left",         -- left or right side
+        },
+        renderer = {
+          highlight_git = true,
+          icons = {
+            show = {
+              git = true,
+              folder = true,
+              file = true,
+            },
+          },
+        },
+        filters = {
+          dotfiles = false,      -- show or hide dotfiles
+          git_ignored = false,   -- show or hide git_ignored files
+        },
+        git = {
+          enable = true,         -- show git status icons
+        },
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'auto',
+          component_separators = { left = '|', right = '|'},
+          section_separators = { left = '', right = ''},
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          }
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+      }
+    end,
+  },
+}
+
