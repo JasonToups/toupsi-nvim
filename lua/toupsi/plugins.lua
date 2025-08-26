ea7c134d2220e5c15621c3b23ad9fa3c4ff7788c
jasontoups <me@jasontoups.com>
2025-08-26 11:37:43 -0700

    added nvim truee to file

--- lua/toupsi/plugins.lua
+++ lua/toupsi/plugins.lua
@@ -1,32 +1,6 @@
 return {
   {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup({
-        view = {
-          width = 35,            -- set tree width
-          side = "left",         -- left or right side
-        },
-        renderer = {
-          highlight_git = true,
-          icons = {
-            show = {
-              git = true,
-              folder = true,
-              file = true,
-            },
-          },
-        },
-        filters = {
-          dotfiles = false,      -- show or hide dotfiles
-          git_ignored = false,   -- show or hide git_ignored files
-        },
-        git = {
-          enable = true,         -- show git status icons
-        },
-      })
-    end,
+
   },
 }
 
